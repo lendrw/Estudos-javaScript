543c0522ce8e4eb1c785ef0abc23c0399d38edd7
lendrw <leandro.cabral@alunos.ufersa.edu.br>
2022-09-12 16:48:31 -0300

    Update main.js

--- Udemy/Seção 6 - objetos e prototypes/A92 métodos úteis para objetos/main.js
+++ Udemy/Seção 6 - objetos e prototypes/A92 métodos úteis para objetos/main.js
@@ -1,4 +1,14 @@
 const produto = {nome: 'produto', preco: 1.8};
 const caneca = produto;
+caneca.nome = 'caneca'
 
+console.log(caneca);
 console.log(produto);
+// dessa forma, alterando um dos objetos, ambos serão alterados, pois se trata do mesmo endereço de memória
+
+const copo = {...produto, material: 'plástico'};
+copo.nome = 'copo';
+console.log(copo);
+//usando spread operator essa problema não ocorre
+
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome')); //retorna as propriedades
