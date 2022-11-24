923d30ee7262b4184c914f7d1a48710a7bf48c03
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2022-11-24 11:51:00 -0300

    Create main.js

---
+++ Udemy/Seção 6 - objetos e prototypes/A99 polimorfismo/main.js
@@ -0,0 +1,30 @@
+//superclass
+function Conta(agencia, conta, saldo) {
+    this.agencia = agencia;
+    this.conta = conta;
+    this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function(valor){
+    if(this.saldo < valor) {
+        console.log(`Saldo insuficiente: ${this.saldo}`)
+        return;
+    }
+    this.saldo -= valor;
+    this.verSaldo();
+};
+Conta.prototype.depositar = function(valor){
+    this.saldo += valor;
+    this.verSaldo();
+};
+Conta.prototype.verSaldo = function(){
+    console.log(
+        `Ag/c: ${this.agencia}/${this.conta} | ` +
+        `Saldo: R$${this.saldo.toFixed(2)}`
+    );
+};
+
+const conta1 = new Conta(11, 22, 10);
+conta1.depositar(11);
+conta1.sacar(2);
+conta1.sacar(20);
