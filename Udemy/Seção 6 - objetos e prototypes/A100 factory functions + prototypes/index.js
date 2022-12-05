75db62aca52f41f3f17dc0ce60dfac1a58c47418
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2022-12-04 22:30:44 -0300

    .

---
+++ Udemy/Seção 6 - objetos e prototypes/A100 factory functions + prototypes/index.js
@@ -0,0 +1,33 @@
+//o objetivo é desacoplar os métodos do objetos do método
+//composição ou mixing
+
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  },
+};
+
+const comer = {
+  comer() {
+    console.log(`${this.nome} está comendo.`);
+  },
+};
+
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo.`);
+  },
+};
+
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome }
+  });
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio');
+const p2 = criaPessoa('Maria', 'A.');
+console.log(p2);
--- Udemy/Seção 6 - objetos e prototypes/A99 polimorfismo/main.js
+++ Udemy/Seção 6 - objetos e prototypes/A99 polimorfismo/main.js
@@ -13,10 +13,12 @@ Conta.prototype.sacar = function(valor){
     this.saldo -= valor;
     this.verSaldo();
 };
+
 Conta.prototype.depositar = function(valor){
     this.saldo += valor;
     this.verSaldo();
 };
+
 Conta.prototype.verSaldo = function(){
     console.log(
         `Ag/c: ${this.agencia}/${this.conta} | ` +
@@ -24,7 +26,39 @@ Conta.prototype.verSaldo = function(){
     );
 };
 
-const conta1 = new Conta(11, 22, 10);
-conta1.depositar(11);
-conta1.sacar(2);
-conta1.sacar(20);
+function ContaCorrente(agencia, conta, saldo, limite) {
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function(valor){
+    if(valor > (this.saldo + this.limite)) {
+        console.log(`Saldo insuficiente: ${this.saldo}`)
+        return;
+    }
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+function ContaPoupanca(agencia, conta, saldo, limite) {
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
+const cc = new ContaCorrente(11, 22, 0, 100);
+cc.depositar(10);
+cc.sacar(90);
+cc.sacar(90);
+
+console.log();
+
+const cp = new ContaPoupanca(12, 33, 0);
+cp.depositar(10);
+cp.sacar(90);
+cp.sacar(90);
