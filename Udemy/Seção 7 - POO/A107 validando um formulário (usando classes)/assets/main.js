c2a55bd12052fc2ad1961ac8923e5c7805fe61b5
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2022-12-17 03:04:31 -0300

    Update main.js

--- Udemy/Seção 7 - POO/A107 validando um formulário (usando classes)/assets/main.js
+++ Udemy/Seção 7 - POO/A107 validando um formulário (usando classes)/assets/main.js
@@ -1,52 +1,113 @@
 class ValidaFormulario {
-    constructor() {
-        this.formulario = document.querySelector('.formulario');
-        this.eventos();
+  constructor() {
+    this.formulario = document.querySelector('.formulario');
+    this.eventos();
+  }
+
+  eventos() {
+    this.formulario.addEventListener('submit', e => {
+      this.handleSubmit(e);
+      //impede que o evento ocorra
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const camposValidos = this.camposSaoValidos();
+    const senhasValidas = this.senhasSaoValidas();
+
+    if(camposValidos && senhasValidas) {
+      alert('Formulário enviado.');
+      this.formulario.submit();
+      //libera o envio do formulário após a validação
+    }
+  }
+
+  senhasSaoValidas() {
+    let valid = true;
+
+    const senha = this.formulario.querySelector('.senha');
+    const repetirSenha = this.formulario.querySelector('.repetir-senha');
+
+    if(senha.value !== repetirSenha.value) {
+      valid = false;
+      this.criaErro(senha, 'Campos senha e repetir senha precisar ser iguais.');
+      this.criaErro(repetirSenha, 'Campos senha e repetir senha precisar ser iguais.');
     }
 
-    eventos() {
-        this.formulario.addEventListener('submit', e => {
-            this.handleSubmit(e);
-        });
+    if(senha.value.length < 6 || senha.value.length > 12) {
+      valid = false;
+      this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.');
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        const camposValidos = this.isValid();
+    return valid;
+  }
+
+  camposSaoValidos() {
+    let valid = true;
+
+    for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+      errorText.remove();
+      //remove os erros caso tudo esteja válido
     }
 
-    isValid() {
-        let valid = true;
+    for(let campo of this.formulario.querySelectorAll('.validar')) {
+      const label = campo.previousElementSibling.innerText;
+      //pega o label dos inputs
 
-        for(let errorText of this.formulario.querySelectorAll('.error-text')) {
-            errorText.remove();
-        }
+      if(!campo.value) {
+        this.criaErro(campo, `Campo "${label}" não pode estar em branco.`);
+        valid = false;
+      }
 
-        for (let campo of this.formulario.querySelectorAll('.validar')) {
-            const label = campo.previousElementSibling.innerText;
+      if(campo.classList.contains('cpf')) {
+        if(!this.validaCPF(campo)) valid = false;
+      }
 
-            if(!campo.value) {
-                this.criaErro(campo, `Campo "${label}" não pode estar em branco.`);
-                valid = false;
-            }
+      if(campo.classList.contains('usuario')) {
+        if(!this.validaUsuario(campo)) valid = false;
+      }
 
-            if(campo.classList.contains('cpf')) {
-                if(!this.validaCPF(campo)) valid = false;
-            }
-        }
     }
 
-    validaCPF(campo) {
-        const cpf = new this.validaCPF(cpf.value);
-        
+    return valid;
+  }
+
+  validaUsuario(campo) {
+    const usuario = campo.value;
+    let valid = true;
+
+    if(usuario.length < 3 || usuario.length > 12) {
+      this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres.');
+      valid = false;
     }
 
-    criaErro(campo, msg) {
-        const div = document.createElement('div');
-        div.innerHTML = msg;
-        div.classList.add('error-text');
-        campo.insertAdjacentElement('afterend', div)
+    if(!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+      this.criaErro(campo, 'Nome de usuário precisar conter apenas letras e/ou números.');
+      valid = false;
     }
+
+    return valid;
+  }
+
+  validaCPF(campo) {
+    const cpf = new ValidaCPF(campo.value);
+
+    if(!cpf.valida()) {
+      this.criaErro(campo, 'CPF inválido.');
+      return false;
+    }
+
+    return true;
+  }
+
+  criaErro(campo, msg) {
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    div.classList.add('error-text');
+    campo.insertAdjacentElement('afterend', div);
+    //cria a div com o erro após o elemento selecionado
+  }
 }
 
 const valida = new ValidaFormulario();
