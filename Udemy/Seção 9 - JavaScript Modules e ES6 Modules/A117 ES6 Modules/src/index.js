61ed35d37256be5514b1e226fb4f8f30c0ac950d
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2022-12-31 17:49:45 -0300

    .

---
+++ Udemy/Seção 9 - JavaScript Modules e ES6 Modules/A117 ES6 Modules/src/index.js
@@ -0,0 +1,8 @@
+import soma2, {nome as nome2, idade} from './modulo1';
+//importa o default
+//import * from './modulo1';
+const nome = 'Hiago';
+
+console.log(nome2, idade);
+console.log(nome);
+console.log(soma2(1, 2));
---
+++ Udemy/Seção 9 - JavaScript Modules e ES6 Modules/A117 ES6 Modules/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Leandro';
+const sobrenome = 'Freire';
+export const idade = 21;
+
+export default function soma(x, y) {
+    return x + y;
+}
+
+export {nome, soma as soma2};
