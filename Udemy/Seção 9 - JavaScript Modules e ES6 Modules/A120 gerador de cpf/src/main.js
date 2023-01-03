308bc8958ae9cb8aae478109b01a4324d8ee7b18
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2023-01-03 18:54:10 -0300

    .

---
+++ Udemy/Seção 9 - JavaScript Modules e ES6 Modules/A120 gerador de cpf/src/main.js
@@ -0,0 +1,8 @@
+import './assets/css/style.css';
+import GeraCPF from './modules/GeraCPF';
+
+(function() {
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = gera.geraNovoCpf();
+})();
