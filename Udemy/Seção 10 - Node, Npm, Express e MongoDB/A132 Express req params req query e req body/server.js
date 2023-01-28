0a86c5e0345f48e641a340b6b1d387509a91f697
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2023-01-28 14:17:56 -0300

    Update server.js

--- Udemy/Seção 10 - Node, Npm, Express e MongoDB/A132 Express req params req query e req body/server.js
+++ Udemy/Seção 10 - Node, Npm, Express e MongoDB/A132 Express req params req query e req body/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const app = express();
 
-
+app.use(express.urlencoded(
+        { 
+        extended: true
+        }
+    )
+);
 
 app.get('/', (req, res) => {
     res.send(`
     <form action="/" method="POST">
-    Nome: <input type="text"></input>
+    Nome do cliente: <input type="text" name="nome"></input>
     <button>Enviar</button>
     </form>
     `);
 });
 
-app.get('/contato', (req, res) => {
-    res.send("Recebi o formulário");
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+    //http://localhost:3000/testes/?nome=Leandro&sobrenome=Freire&idade=21
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.query);
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(`O que você me enviou foi: ${req.body.nome}`);
 }
 );
 
