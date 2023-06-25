863ca09fa4c66e004456d73e70bd4d5a3bf4e6fe
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2023-06-25 19:44:22 -0300

    .

--- Udemy/Seção 11 - projeto agenda com Express e MongoDB/src/models/LoginModel.js
+++ Udemy/Seção 11 - projeto agenda com Express e MongoDB/src/models/LoginModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  senha: { type: String, required: true }
+  password: { type: String, required: true }
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -15,14 +16,40 @@ class Login {
     this.user = null;
   }
 
+  async login () {
+    this.validate();
+    if(this.errors.length > 0) return;
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if(!this.user) {
+      this.errors.push('Usuário não existe.');
+      return;
+    }
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida');
+      this.user = null;
+    }
+  }
+
   async register() {
     this.validate();
     if(this.errors.length > 0) return;
-    try {
-      this.user = await LoginModel.create(this.body);
-    } catch(err) {
-      console.log(err);
-    }
+
+    await this.userExists();
+
+    if(this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+    this.user = await LoginModel.create(this.body);
+
+  }
+
+  async userExists() {
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if(this.user) this.errors.push('Usuário já existe.');
   }
 
   validate() {
