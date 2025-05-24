061897d4c759f9ce44b4ab19227d62315acc5304
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-05-24 18:23:30 -0300

    .

---
+++ Udemy/ReactGram/backend/routes/UserRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+// Controller
+const { register, login, getCurrentUser } = require("../controllers/UserController");
+
+// Midlewares
+const validate = require("../middlewares/handleValidation");
+const { 
+    userCreateValidation,
+    loginValidation,
+} = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
+
+// Routes
+router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser)
+
+module.exports = router;
