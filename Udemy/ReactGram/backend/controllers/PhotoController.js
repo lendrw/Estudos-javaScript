afe0ccfd2aa6831a0022cabc87ce39d49430f6bd
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-05-26 17:28:54 -0300

    .

--- Udemy/ReactGram/backend/controllers/PhotoController.js
+++ Udemy/ReactGram/backend/controllers/PhotoController.js
@@ -36,9 +36,122 @@ const deletePhoto = async(req, res) => {
 
     const reqUser = req.user;
 
-    const photo = Photo.findById();
+    try {
+        const photo = await Photo.findById(id);
+
+        //Check if photo exists
+        if (!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada."] });
+            return;
+        };
+
+        //Check if photo belongs to user
+        if(!photo.userId.equals(reqUser._id)) {
+            res
+                .status(422)
+                .json({ 
+                    errors: ["Ocorreu um erro, por favor tente mais tarde."], 
+                });
+        };
+
+        await Photo.findByIdAndDelete(photo._id);
+
+        res
+            .status(200)
+            .json({ id: photo._id, message: "Foto excluída com sucesso." });
+    } catch (error) {
+        res
+            .status(404)
+            .json({ errors: "Foto não encontrada." });
+        return;
+    }
+};
+
+//Get all photos
+const getAllPhotos = async (req, res) => {
+    try {
+        const photos = await Photo.find({})
+            .sort([["createdAt", -1]])
+            .exec();
+
+        if (!photos || photos.length === 0) {
+            return res.status(404).json({ errors: ["Nenhuma foto encontrada."] });
+        }
+
+        return res.status(200).json(photos);
+    } catch (error) {
+        console.error("Erro ao buscar fotos:", error);
+        return res.status(500).json({ errors: ["Erro ao buscar as fotos."] });
+    }
+};
+
+//Get user photos
+const getUserPhotos = async (req, res) => {
+    const { id } = req.params;
+
+    const photos = await Photo.find({ userId: id})
+        .sort([["createAt", -1]])
+        .exec();
+
+    return res.status(200).json(photos);
+};
+
+//Get photo by id
+const getPhotoById = async (req, res) => {
+
+    const {id} = req.params;
+
+    const photo = await Photo.findById(id);
+
+    //Check if photo exists
+    if(!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada."] });
+        return;
+    }
+
+    res.status(200).json(photo);
+};
+
+//Update a photo
+const updatePhoto = async(req, res) => {
+
+    const {id} = req.params;
+    const {title} = req.body;
+
+    const reqUser = req.user;
+
+    const photo = await Photo.findById(id);
+
+    //Check if photo exists
+    if(!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada."] });
+        return;
+    }
+
+    //Check if photo belongs to user
+    if(!photo.userId.equals(reqUser._id)) {
+        res
+            .status(422)
+            .json({ 
+                errors: ["Ocorreu um erro, por favor tente mais tarde."], 
+            });
+        return;
+    }
+
+    if(title) {
+        photo.title = title;
+    }
+
+    await photo.save();
+
+    res.status(200).json({photo, message: "Foto atualizada com sucesso."});
 }
 
 module.exports = {
     insertPhoto,
-}
+    deletePhoto,
+    getAllPhotos,
+    getUserPhotos,
+    getPhotoById,
+    updatePhoto,
+};
