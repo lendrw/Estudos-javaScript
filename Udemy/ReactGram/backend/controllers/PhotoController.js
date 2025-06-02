8e5254cab403f433fbcf3c363c67f3606310655c
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-06-02 11:18:25 -0300

    .

--- Udemy/ReactGram/backend/controllers/PhotoController.js
+++ Udemy/ReactGram/backend/controllers/PhotoController.js
@@ -147,6 +147,85 @@ const updatePhoto = async(req, res) => {
     res.status(200).json({photo, message: "Foto atualizada com sucesso."});
 }
 
+// Like functionality
+const likePhoto = async(req, res) => {
+
+    const {id} = req.params;
+
+    const reqUser = req.user;
+
+    const photo = await Photo.findById(id);
+
+    //Check if photo exists
+    if(!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada."] });
+        return;
+    }
+
+    // Check if user already exists
+    if (photo.likes.includes(reqUser._id)) {
+        res.status(422).json({ errors: ["Você já curtiu a foto."] });
+        return;
+    }
+
+    // Put user id in likes array
+    photo.likes.push(reqUser._id)
+
+    photo.save()
+
+    res
+        .status(200)
+        .json({ photoId: id, userId: reqUser._id, message: "A foto foi curtida." });
+}
+
+// Comment functionality
+const commentPhoto = async(req, res) => {
+
+    const {id} = req.params;
+    const {comment} = req.body;
+
+    const reqUser = req.user;
+
+    const user = await User.findById(reqUser._id);
+
+    const photo = await Photo.findById(id);
+
+    //Check if photo exists
+    if(!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada."] });
+        return;
+    }
+
+    // Put comment in the array comments
+    const userComment = {
+        comment,
+        userName: user.name,
+        userImage: user.profileImage,
+        userId: user._id,
+    };
+
+    photo.comments.push(userComment);
+
+    await photo.save()
+
+    res.status(200).json({
+        comment: userComment,
+        message: "O comentário foi adicionado com sucesso.",
+    });
+}
+
+// Search photos by title
+const searchPhotos = async(req, res) => {
+
+    const {q} = req.query;
+
+    const photos = await Photo.find({title: new RegExp(q, "i")}).exec()
+
+    res.status(200).json(photos);
+
+
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -154,4 +233,7 @@ module.exports = {
     getUserPhotos,
     getPhotoById,
     updatePhoto,
+    likePhoto,
+    commentPhoto,
+    searchPhotos,
 };
