e8f35a9597e3d7a3128ab1bc0339db95d98f2c9f
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-06-11 16:02:48 -0300

    .

--- Udemy/ReactGram/frontend/src/pages/Profile/Profile.jsx
+++ Udemy/ReactGram/frontend/src/pages/Profile/Profile.jsx
@@ -12,6 +12,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import { getUserDetails } from "../../slices/userSlice";
+import { publishPhoto, resetMessage } from "../../slices/photoSlice";
 
 const Profile = () => {
   const { id } = useParams();
@@ -20,15 +21,103 @@ const Profile = () => {
 
   const { user, loading } = useSelector((state) => state.user);
   const { user: userAuth } = useSelector((state) => state.auth);
+  const {
+    photos,
+    loading: loadingPhoto,
+    message: messagePhoto,
+    error: errorPhoto,
+  } = useSelector((state) => state.photo);
 
-  //photo
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+
+  //new form and edit form ref
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
 
   //load user data
   useEffect(() => {
-    dispatch(getUserDetails(id))
-  }, [dispatch, id])
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
 
-  return <div></div>;
+  const handleFile = (e) => {
+    //image preview
+    const image = e.target.files[0];
+
+    setImage(image);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const photoData = {
+      title,
+      image,
+    };
+
+    //bild form data
+    const formData = new FormData();
+
+    const photoFormData = Object.keys(photoData).forEach((key) =>
+      formData.append(key, photoData[key])
+    );
+
+    formData.append("photo", photoFormData);
+
+    dispatch(publishPhoto(formData));
+
+    setTitle("");
+
+    setTimeout(() => {
+      dispatch(resetMessage());
+    }, 2000);
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={handleSubmit}>
+              <label>
+                <span>Título para a foto:</span>
+                <input
+                  type="text"
+                  placeholder="Insira um título"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title || ""}
+                />
+              </label>
+              <label>
+                <span>Imagem:</span>
+                <input type="file" onChange={handleFile} />
+              </label>
+              {!loadingPhoto && <input type="submit" value="Publicar" />}
+              {loadingPhoto && (
+                <input type="submit" value="Aguarde..." disabled />
+              )}
+            </form>
+          </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error"/>}
+          {messagePhoto && <Message msg={messagePhoto} type="success" />}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
---
+++ Udemy/ReactGram/frontend/src/services/photoService.js
@@ -0,0 +1,35 @@
+import { api, requestConfig } from "../utils/config";
+
+//publish user photo
+const publishPhoto = async (data, token) => {
+  const config = requestConfig("POST", data, token, true);
+
+  try {
+    const res = await fetch(api + "/photos", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//get user photos
+const getUserPhotos = async (id, token) => {
+  const config = requestConfig("GET", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const photoService = { publishPhoto, getUserPhotos };
+
+export default photoService;
