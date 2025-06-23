74122d78778a02e39a7064a665962862e78fa792
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-06-23 16:32:31 -0300

    .

---
+++ ReactNative/onebithealth/src/components/Form/index.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import ResultImc from "./ResultImc";
+import styles from "./style";
+
+export default function Form() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [messageImc, setMessageImc] = useState("Preencha o peso e altura");
+  const [imc, setImc] = useState(null);
+  const [textButton, setTextButton] = useState("Calcular IMC");
+
+  function imcCalculator() {
+    return setImc((weight / (height * height)).toFixed(2));
+  }
+
+  function validationImc() {
+    if (height != null && weight != null) {
+      imcCalculator();
+      setHeight(null);
+      setWeight(null);
+      setMessageImc("Seu IMC é igual: ");
+      setTextButton("Calcular novamente");
+      return;
+    }
+    setImc(null);
+    setTextButton("Calcular IMC");
+    setMessageImc("Preencha o peso e altura");
+  }
+
+  return (
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setHeight}
+          value={height}
+          placeholder="Ex. 1.70"
+          keyboardType="numeric"
+        />
+        <Text style={styles.formLabel}>Peso</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setWeight}
+          value={weight}
+          placeholder="Ex. 70.5"
+          keyboardType="numeric"
+        />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => validationImc()}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
+      </View>
+      <ResultImc messageResultImc={messageImc} resultImc={imc} />
+    </View>
+  );
+}
