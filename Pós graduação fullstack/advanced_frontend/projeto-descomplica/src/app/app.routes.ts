5a93a1c57a89719cf73c6c467a7dd77424c266ee
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-07-21 11:20:27 -0300

    .

--- Pós graduação fullstack/advanced_frontend/projeto-descomplica/src/app/app.routes.ts
+++ Pós graduação fullstack/advanced_frontend/projeto-descomplica/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/auth/login.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
