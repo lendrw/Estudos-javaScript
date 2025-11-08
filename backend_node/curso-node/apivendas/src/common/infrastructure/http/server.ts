5beca5e547deea6323b591051df8148d59b6ed69
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-11-08 15:14:18 -0300

    .

--- backend_node/curso-node/apivendas/src/common/infrastructure/http/server.ts
+++ backend_node/curso-node/apivendas/src/common/infrastructure/http/server.ts
@@ -1,7 +1,15 @@
 import { env } from '../env'
+import { dataSource } from '../typeorm'
 import { app } from './app'
 
-app.listen(env.PORT, () => {
-  console.log(`Server is runnig on port ${env.PORT}`)
-  console.log(`API docs avaliable at GET /docs`)
-})
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(env.PORT, () => {
+      console.log(`Server is runnig on port ${env.PORT}`)
+      console.log(`API docs avaliable at GET /docs`)
+    })
+  })
+  .catch(error => {
+    console.error('Error during Data Source initialization', error)
+  })
