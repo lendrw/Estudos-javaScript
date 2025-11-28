7ddcd43b3e80adade7531e46cf3c4957e14a550e
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-11-28 15:47:54 -0300

    .

--- backend_node/curso-node/apivendas/src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int-spec.ts
+++ backend_node/curso-node/apivendas/src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int-spec.ts
@@ -3,6 +3,7 @@ import { ProductsTypeormRepository } from './products-typeorm.repository'
 import { Product } from '../entities/products.entity'
 import { NotFoundError } from '@/common/domain/errors/not-found-error'
 import { randomUUID } from 'node:crypto'
+import { ProductsDataBuilder } from '../../testing/helpers/products-data-builder'
 
 describe('ProductsTypeormRepository integration tests', () => {
   let ormRepository: ProductsTypeormRepository
@@ -27,6 +28,32 @@ describe('ProductsTypeormRepository integration tests', () => {
       await expect(ormRepository.findById(id)).rejects.toThrow(
         new NotFoundError(`Product not found using ID ${id}`),
       )
+    })
+
+    it('should find a product by ID', async () => {
+      const data = ProductsDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      const result = await ormRepository.findById(product.id)
+      expect(result.id).toEqual(product.id)
+      expect(result.name).toEqual(product.name)
+    })
+  })
+
+  describe('create', () => {
+    it('should create a new product object', () => {
+      const data = ProductsDataBuilder({ name: 'Product 1' })
+      const result = ormRepository.create(data)
+      expect(result.name).toEqual(data.name)
+    })
+  })
+
+  describe('insert', () => {
+    it('should insert a new product', async () => {
+      const data = ProductsDataBuilder({ name: 'Product 1' })
+      const result = await ormRepository.insert(data)
+      expect(result.name).toEqual(data.name)
     })
   })
 })
